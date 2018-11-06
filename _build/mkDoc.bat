9644d0aed573f2a02c9b50bd28638935fae6b7d6
hoppfrosch <hoppfrosch@gmx.de>
2018-11-06 06:46:37 +0100

    Auto generation of documentation on each commit

---
+++ _build/mkDoc.bat
@@ -0,0 +1,9 @@
+REM @echo off
+echo.
+echo Creating documentation....
+echo.
+
+::path to the natural doc folder
+SET NDPATH=c:\Usr\Programme\NaturalDocs
+
+call c:\Usr\Programme\NaturalDocs\NaturalDocs -r -p .\NDProj
